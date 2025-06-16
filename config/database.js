c8933dd9131eb1365157c641b7aef4bd2dd283dd
Rajin <marajin57@gmail.com>
2025-06-16 16:02:59 +0600

    Nothing

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const dbConfig = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'webuzo_saas',
+    database: process.env.DB_NAME || 'hospx_saas',
     multipleStatements: true
 };
 
